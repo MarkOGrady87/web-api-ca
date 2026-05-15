a5028bc1ae5b5f5edbd8972b36a5147c50aac5dd
markogrady10 <20107942@mail.wit.ie>
2026-05-15 21:20:59 +0100

    start page styled and added description

--- react-movies/src/pages/startPage.jsx
+++ react-movies/src/pages/startPage.jsx
@@ -1,14 +1,26 @@
 import { useContext } from "react";
 import { AuthContext } from '../contexts/authContext';
 import { Link } from "react-router";
+import { Grid, Typography } from "@mui/material";
 
 const StartPage = () => {
-  
+
     return (
-        <p>
-            <Link to="/login">Login</Link> or <Link to="/signup">Signup</Link> to create tasks!
-        </p>
+        <>
+            <Grid container>
+                <Grid size={12} justifyContent={"center"} display={"flex"} mt={5}>
+                    <Typography variant="h2">Welcome</Typography>
+                </Grid>
+                <Grid size={12} justifyContent={"center"} display={"flex"} mt={12}>
+                    <Typography variant="h5">
+                        Welcome to the TMDB Client application, your place to discover movies,
+                        explore actor profiles, browse reviews and keep track of your favourite films.
+                        Search through popular, top rated and upcoming movies while building your own personalised watchlist.
+                    </Typography>
+                </Grid>
+            </Grid>
+        </>
     );
-  };
+};
 
 export default StartPage;
